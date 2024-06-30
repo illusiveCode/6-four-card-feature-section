6e5123e47442ddf353253a83aed48647637544cd
Muhammad Raza <mh.raza2906@gmail.com>
2024-06-30 16:19:25 +0100

    chore: update image imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-// import react from "react";
 import "./global.css";
 import Card from "./components/Card";
+import supervisorIcon from "./assets/icon-supervisor.svg";
+import teamBuilderIcon from "./assets/icon-team-builder.svg";
+import karmaIcon from "./assets/icon-karma.svg";
+import calculatorIcon from "./assets/icon-calculator.svg";
 
 const App = () => {
   return (
     <div className="h-screen bg-neutral-light-gray">
       <div className="container">
-        <div className="max-w-[600px] mx-auto text-center  mb-24">
+        <div className="max-w-[600px] mx-auto text-center mb-24">
           <h1 className="text-xl font-thin mobile:text-2xl tablet:text-3xl laptop:text-4xl desktop:text-5xl mb-6 text-neutral-dark-blue">
             Reliable, efficient delivery <br />
             <span className="font-semibold leading-relaxed text-neutral-dark-blue">
@@ -18,28 +21,28 @@ const App = () => {
             that your project is successful
           </p>
         </div>
-        <section className=" grid grid-cols-1 gap-6 laptop:grid-cols-3">
+        <section className="grid grid-cols-1 gap-6 laptop:grid-cols-3">
           <div className="self-center">
             <Card
               title="Supervisor"
               text="Monitors activity to identify project roadblocks"
               borderColour="primary-cyan"
-              icon={"../images/icon-supervisor.svg"}
+              icon={supervisorIcon}
             />
           </div>
 
           <div className="box-container flex flex-col gap-6">
             <Card
+              borderColour="primary-red"
               title="Team Builder"
               text="Scans our talent network to create the optimal team for your project"
-              borderColour="primary-red"
-              icon={"../images/icon-team-builder.svg"}
+              icon={teamBuilderIcon}
             />
             <Card
               title="Karma"
               text="Regularly evaluates our talent to ensure quality"
               borderColour="primary-orange"
-              icon={"../images/icon-karma.svg"}
+              icon={karmaIcon}
             />
           </div>
           <div className="self-center">
@@ -47,7 +50,7 @@ const App = () => {
               title="Calculator"
               text="Uses data from past projects to provide better delivery estimates"
               borderColour="primary-blue"
-              icon={"../images/icon-calculator.svg"}
+              icon={calculatorIcon}
             />
           </div>
         </section>
